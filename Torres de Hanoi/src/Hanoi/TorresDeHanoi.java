577fc31ab13add2a2791530b2ac5df7a1d84a31c
luisbaquiax <47260039+luisbaquiax@users.noreply.github.com>
2020-11-06 01:04:15 -0600

    metodo cantidadMovimientos

--- Torres de Hanoi/src/Hanoi/TorresDeHanoi.java
+++ Torres de Hanoi/src/Hanoi/TorresDeHanoi.java
@@ -9,11 +9,10 @@ import java.util.ArrayList;
 
 /**
  * Clase que se encarga de la logica del juego torres de hanoi
- * 
+ *
  */
 public class TorresDeHanoi implements Serializable {
 
-    
     String cadena;
     ArrayList<String> cadenas;
     //guarda el numero de pasos completos
@@ -21,60 +20,76 @@ public class TorresDeHanoi implements Serializable {
 
     public TorresDeHanoi() {
         cadena = "";
-        cadenas= new ArrayList<>();
-        contador=0;
+        cadenas = new ArrayList<>();
+        contador = 0;
     }
 
-    
     public void hanoi(int num, int inicio, int auxiliar, int fin) throws IOException {
         if (num == 1) {
             cadena = "MOVER DE LA TORRE " + inicio + " A LA TORRE " + fin;
             contador++;
             cadenas.add(cadena);
-            
+
         } else {
             hanoi(num - 1, inicio, fin, auxiliar);
             cadena = "MOVER DE LA TORRE " + inicio + " A LA TORRE " + fin;
             contador++;
             cadenas.add(cadena);
-            
+
             hanoi(num - 1, auxiliar, inicio, fin);
         }
     }
 
+    /**
+     * Calcula la cantidad de movimientos que se requiere para mover de una
+     * torre a otra
+     *
+     * @param numeroDiscos cantidad de discos que el usuario quiera ingresar
+     * @return cantidad de movimientos
+     */
+    public int cantidadMovimientosMinimos(int numeroDiscos) {
+        int resultado = 2;
+        for (int i = 1; i < numeroDiscos; i++) {
+            resultado = resultado * 2;
+        }
+        return resultado;
+    }
+
     public void guardarSolucion(File guarda) throws IOException {
-        PrintWriter salida=null;
-        try{
-            FileWriter fileWriter = new FileWriter(guarda+".txt");
+        PrintWriter salida = null;
+        try {
+            FileWriter fileWriter = new FileWriter(guarda + ".txt");
             salida = new PrintWriter(fileWriter);
-             
-            for (String cad : cadenas){
+
+            for (String cad : cadenas) {
                 salida.println(cad);
                 salida.println();
             }
-            
-        }finally{
-            if (salida != null){
+
+        } finally {
+            if (salida != null) {
                 salida.close();
             }
         }
-        
+
     }
-    
+
     /**
      * Meotodo para verificar si ya acabo el juego
+     *
      * @param n
      * @param numeroArosTorre3
-     * @return 
+     * @return
      */
-    public boolean verificarFinalJuego(int n, int numeroArosTorre3){
-        
-        return n==numeroArosTorre3;
+    public boolean verificarFinalJuego(int n, int numeroArosTorre3) {
+
+        return n == numeroArosTorre3;
     }
 
     /**
      * Metodo que retorna el minimo de movimientos segun el numero de aros
-     * @return 
+     *
+     * @return
      */
     public int getContador() {
         return contador;
@@ -82,11 +97,11 @@ public class TorresDeHanoi implements Serializable {
 
     /**
      * Metodo para setear el contador
-     * @param contador 
+     *
+     * @param contador
      */
     public void setContador(int contador) {
         this.contador = contador;
     }
-    
-    
+
 }
